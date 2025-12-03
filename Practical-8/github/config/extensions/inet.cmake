f0d31f6072d33f74976cb693db0f11519f53345a
joodessa43-hue <joodessa43@gmail.com>
2025-12-03 11:05:50 +0300

    Add files via upload

---
+++ Practical-8/github/config/extensions/inet.cmake
@@ -0,0 +1,8 @@
+duckdb_extension_load(inet
+    LOAD_TESTS
+    GIT_URL https://github.com/duckdb/duckdb-inet
+    GIT_TAG f6a2a14f061d2dfccdb4283800b55fef3fcbb128
+    INCLUDE_DIR src/include
+    TEST_DIR test/sql
+    APPLY_PATCHES
+    )
